aa570be377255b241f45a35ce9ab06f3d5263ff3
santiago <sntglnmndz@gmail.com>
2024-02-18 18:18:41 -0500

    created models

--- inventario/admin.py
+++ inventario/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from inventario.models import *
 
 # Register your models here.
+
+admin.site.register(Categoria)
+admin.site.register(Inventario)
+admin.site.register(Usuario)
+admin.site.register(Prestamos)
--- inventario/models.py
+++ inventario/models.py
@@ -1,3 +1,34 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.db import models
 
-# Create your models here.
+class Categoria(models.Model):
+    nombre=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.nombre
+
+class Inventario(models.Model):
+    codigo=models.BigIntegerField()
+    nombre=models.CharField(max_length=100)
+    categoria=models.ForeignKey(Categoria, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.codigo
+
+class Usuario(models.Model): 
+    cedula=models.IntegerField()
+    nombre=models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.cedula
+
+    
+class Prestamos(models.Model):
+    id_inventario=models.ForeignKey(Inventario,on_delete=models.CASCADE)
+    id_usuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    fecha_prestamo=models.DateTimeField(auto_now_add=True)
+    fecha_devolucion=models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.id_inventario
