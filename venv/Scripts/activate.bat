003df1c4df2e1f7e731a661d1d7ea436747aa1cb
santiago <sntglnmndz@gmail.com>
2024-02-27 20:39:05 -0500

    clonado para laptop

--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\62_Jornada1.DRDDFPRCLFSD007\Desktop\gymsena\venv
+set VIRTUAL_ENV=C:\Users\ASUS\Desktop\gymsena\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
