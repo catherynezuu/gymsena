d762060ae6ff37442d1ebde2b5a7a00d7d569bd4
santiago <sntglnmndz@gmail.com>
2024-04-08 16:00:43 -0500

    agregada funcionalidad para seleccionar préstamo abierto y copiar esa información en el formulario de devoluciones

--- inventario/static/home.js
+++ inventario/static/home.js
@@ -8,6 +8,8 @@ document.addEventListener("DOMContentLoaded", function () {
   const btnSwitchDevoluciones = document.getElementById("switch_devoluciones");
   const formPrestamos = document.getElementById("prestamos_form");
   const formDevoluciones = document.getElementById("devoluciones_form");
+  const cedulaInput = document.querySelector("#devoluciones_form #id_cedula_usuario");
+  const codigoInventarioSelect = document.getElementById("id_codigo_inventario");
 
   botonAgregarUsuario.addEventListener("click", function () {
     modal_agrgar_usuario.style.display = "flex";
@@ -64,4 +66,28 @@ document.addEventListener("DOMContentLoaded", function () {
   // Agregar clase inicial a los botones de switch
   btnSwitchPrestamos.classList.add("button_switch_active");
   btnSwitchDevoluciones.classList.add("button_switch_deactive");
+
+  // Función para copiar la cédula al campo de entrada
+  function copiarCedula(event) {
+    const fila = event.currentTarget;
+    const cedula = fila.cells[2].textContent.trim(); // Obtener el texto de la cédula de la fila clicada
+    cedulaInput.value = cedula; // Copiar la cédula al campo de entrada
+  }
+
+  // Función para seleccionar el elemento en el select del formulario
+  function seleccionarElemento(event) {
+    const fila = event.currentTarget;
+    const codigoElemento = fila.getAttribute("data-element-id"); // Obtener el código del elemento de la fila clicada
+    codigoInventarioSelect.value = codigoElemento; // Seleccionar el elemento en el select del formulario
+  }
+
+  // Asignar el evento clic a cada fila de la tabla
+  filasTabla.forEach(function (fila) {
+    fila.addEventListener("click", function (event) {
+      toggleForm(formDevoluciones, formPrestamos, btnSwitchDevoluciones, btnSwitchPrestamos);
+      copiarCedula(event); // Copiar la cédula al campo de entrada
+      seleccionarElemento(event); // Seleccionar el elemento en el select del formulario
+    });
+  });
 });
+
