fa7e65b651473f530ca9b0e6d5f026c21ce3e4d0
santiago <sntglnmndz@gmail.com>
2024-04-10 22:19:53 -0500

    agregada función de imprimir historial

--- inventario/static/historial.js
+++ inventario/static/historial.js
@@ -1,14 +1,22 @@
-document
-  .getElementById("filter_form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  // Elementos del DOM
+  const botonImprimir = document.getElementById("imprimir_historial");
+  const campoCedula = document.getElementById("input_cedula");
+  const campoFechaInicio = document.getElementById("input_fecha_inicio");
+  const campoFechaFin = document.getElementById("input_fecha_fin");
+  const limpiarDocumento = document.getElementById("clean_document");
+  const limpiarInicio = document.getElementById("clean_start");
+  const limpiarFin = document.getElementById("clean_end");
 
-    const cedula = document.getElementById("input_cedula").value.trim();
-    let fechaInicio = document.getElementById("input_fecha_inicio").value;
-    let fechaFin = document.getElementById("input_fecha_fin").value;
+  // Función para filtrar la tabla
+  const filtrarTabla = () => {
+    const cedula = campoCedula.value.trim();
+    let fechaInicio = campoFechaInicio.value;
+    let fechaFin = campoFechaFin.value;
 
+    // Validar fechas
     if (fechaInicio && fechaFin && fechaFin < fechaInicio) {
-      document.getElementById("input_fecha_fin").value = fechaInicio;
+      campoFechaFin.value = fechaInicio;
       fechaFin = fechaInicio;
     }
 
@@ -18,6 +26,7 @@ document
       const cedulaFila = fila.querySelector("td:nth-child(2)").textContent;
       const fechaPrestamo = fila.getAttribute("data-inicio");
 
+      // Aplicar filtros
       if (cedula && cedulaFila !== cedula) mostrar = false;
 
       if (fechaInicio && !fechaFin && fechaInicio !== fechaPrestamo)
@@ -29,56 +38,115 @@ document
 
       fila.style.display = mostrar ? "" : "none";
     });
+  };
+
+  // Función para imprimir la tabla
+  const imprimirTabla = (tabla, cedula, desde, hasta) => {
+    const ventana = window.open("", "PRINT");
+    ventana.document.write(
+      "<html><head><title>Historial Transacciones</title>"
+    );
+    ventana.document.write(
+      "<style> " +
+        "body{font-size:20px; font-family: Arial, Helvetica, sans-serif;} " +
+        "table{width: 100%; border-collapse: collapse;} " +
+        "table, th, td{border: 1px solid gray;} " +
+        "td{text-align:center;} " +
+        "thead {border-bottom: 2px solid black}" +
+        "</style>"
+    );
+    ventana.document.write("</head><body>");
+
+    ventana.document.write(
+      '<div style="display: grid; grid-template-columns: 1fr 1fr 1fr; margin-bottom: 2em; place-items:center;">'
+    );
+    if (cedula) {
+      ventana.document.write("<div><span>Cédula:</span>" + cedula + "</div>");
+    } else {
+      ventana.document.write("<div><span>Cédula:</span>" + "Todos" + "</div>");
+    }
+    if (desde) {
+      ventana.document.write("<div><span>Desde:</span>" + desde + "</div>");
+    } else {
+      ventana.document.write(
+        "<div><span>Desde:</span>" +
+          document.querySelector(".table_container tbody tr:first-child").getAttribute("data-inicio") +
+          "</div>"
+      );
+    }
+    if (hasta) {
+      ventana.document.write("<div><span>Hasta:</span>" + hasta + "</div>");
+    } else {
+      ventana.document.write(
+        "<div><span>Hasta:</span>" +
+          new Date().toLocaleDateString("es-ES") +
+          "</div>"
+      );
+    }
+    ventana.document.write("</div>");
+    ventana.document.write(tabla.innerHTML);
+    ventana.document.write("</body></html>");
+    ventana.document.close();
+    ventana.focus();
+    ventana.print();
+    ventana.close();
+  };
+
+  // Evento click en el botón de imprimir
+  botonImprimir.addEventListener("click", () => {
+    const tabla = document.querySelector(".table_container");
+    filtrarTabla();
+    imprimirTabla(tabla, campoCedula.value, campoFechaInicio.value, campoFechaFin.value);
   });
 
-document.addEventListener("DOMContentLoaded", function () {
-  const cedula = document.getElementById("input_cedula");
-  const fechaInicio = document.getElementById("input_fecha_inicio");
-  const fechaFin = document.getElementById("input_fecha_fin");
-  const cleanDocument = document.getElementById("clean_document");
-  const cleanStart = document.getElementById("clean_start");
-  const cleanEnd = document.getElementById("clean_end");
+  // Evento submit en el formulario de filtro
+  document
+    .getElementById("filter_form")
+    .addEventListener("submit", function (event) {
+      event.preventDefault();
+      filtrarTabla();
+    });
 
-  const cleanField = (field, button) => {
-    field.value = "";
-    button.style.display = "none";
+  // Función para limpiar un campo y ocultar el botón de limpieza
+  const limpiarCampo = (campo, boton) => {
+    campo.value = "";
+    boton.style.display = "none";
   };
 
-  const displayButton = (field, button) => {
-    if (field.value) button.style.display = "inline-block";
-    else button.style.display = "none";
+  // Función para mostrar u ocultar el botón de limpieza según el contenido del campo
+  const mostrarBoton = (campo, boton) => {
+    if (campo.value) boton.style.display = "inline-block";
+    else boton.style.display = "none";
   };
 
-  cleanDocument.addEventListener("click", () =>
-    cleanField(cedula, cleanDocument)
-  );
-  cleanStart.addEventListener("click", () =>
-    cleanField(fechaInicio, cleanStart)
-  );
-  cleanEnd.addEventListener("click", () => cleanField(fechaFin, cleanEnd));
+  // Eventos de limpieza
+  limpiarDocumento.addEventListener("click", () => limpiarCampo(campoCedula, limpiarDocumento));
+  limpiarInicio.addEventListener("click", () => limpiarCampo(campoFechaInicio, limpiarInicio));
+  limpiarFin.addEventListener("click", () => limpiarCampo(campoFechaFin, limpiarFin));
 
-  cedula.addEventListener("input", () => displayButton(cedula, cleanDocument));
-  fechaInicio.addEventListener("input", () =>
-    displayButton(fechaInicio, cleanStart)
-  );
-  fechaFin.addEventListener("input", () => displayButton(fechaFin, cleanEnd));
+  // Eventos de entrada para mostrar u ocultar botones de limpieza
+  campoCedula.addEventListener("input", () => mostrarBoton(campoCedula, limpiarDocumento));
+  campoFechaInicio.addEventListener("input", () => mostrarBoton(campoFechaInicio, limpiarInicio));
+  campoFechaFin.addEventListener("input", () => mostrarBoton(campoFechaFin, limpiarFin));
 
-  const cleanForm = document.getElementById("clean_form");
-  cleanForm.addEventListener("click", function () {
+  // Evento click en el botón de limpiar formulario
+  const limpiarFormulario = document.getElementById("clean_form");
+  limpiarFormulario.addEventListener("click", function () {
     document.getElementById("filter_form").reset();
     document
       .querySelectorAll("#filter_form section div span")
       .forEach((span) => (span.style.display = "none"));
   });
 
-  const cedulaCell = document.querySelectorAll(
+  // Evento click en celda de cédula para filtrar automáticamente
+  const celdasCedula = document.querySelectorAll(
     ".table_container table tbody tr td:nth-child(2)"
   );
-  cedulaCell.forEach((cell) => {
-    cell.addEventListener("click", function () {
-      const numero = cell.textContent.trim();
-      document.getElementById("input_cedula").value = numero;
-      cleanDocument.style.display = "block";
+  celdasCedula.forEach((celda) => {
+    celda.addEventListener("click", function () {
+      const numero = celda.textContent.trim();
+      campoCedula.value = numero;
+      limpiarDocumento.style.display = "block";
     });
   });
 });
